1ff6fceb3eb3878adfa9801543952cdb6a6e24fd
daniilbatmanov <iambatmanov@gmail.com>
2023-04-04 19:56:46 +0300

    TODO

--- src/ExecuteScriptCommand.java
+++ src/ExecuteScriptCommand.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class ExecuteScriptCommand implements Command {
     private final LabWorkCollection collection;
@@ -10,19 +7,34 @@ public class ExecuteScriptCommand implements Command {
         this.collection = collection;
     }
 
+
     @Override
     public void execute() throws IOException {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("fileName: ");
         String fileName = rd.readLine();
+
+        File file = new File(fileName);
+        while (!file.exists()) {
+            System.out.println("File not found. Please enter a valid file name:");
+            fileName = rd.readLine();
+            file = new File(fileName);
+        }
+
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
         String input;
         while ((input = reader.readLine()) != null) {
             CommandProcessor processor = new CommandProcessor(collection);
-            processor.executeCommand(input);
+            if (input.charAt(0) == '/') {
+                System.out.println(input.substring(1));
+            }
+            else {
+                processor.executeCommand(input);
+            }
         }
         reader.close();
     }
+
 
     @Override
     public String getDescription() {
